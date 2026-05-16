cea1793d27fe44476cf45555c52f571faba3fe43
Matthew Cawood <matthewcawood@Matthews-MacBook-Pro.local>
2026-05-16 11:25:50 -0400

    Re-enable cloud recording and max_participants (paid plan)

--- netlify/edge-functions/daily-api.js
+++ netlify/edge-functions/daily-api.js
@@ -78,6 +78,8 @@ export default async (request) => {
           start_video_off: false,
           start_audio_off: false,
           enable_screenshare: true,
+          enable_recording: "cloud",
+          max_participants: 200,
           exp: Math.floor(Date.now() / 1000) + 60 * 60 * 6, // expires 6h from now
         }
       })
